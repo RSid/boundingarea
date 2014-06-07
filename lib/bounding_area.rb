0ed02b65b663c66d0be3b98a9c1422a947d71edc
RSid <sm1th303@gmail.com>
2014-06-07 15:51:52 -0400

    All tests passing

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,18 @@
 class BoundingArea
+
+  def initialize (boxes)
+    @boxes = boxes
+  end
+
+
+  def contains_point?(x,y)
+    result = false
+    @boxes.each do |box|
+      if box.contains_point?(x,y)
+        result = true
+      end
+    end
+    result
+  end
+
 end
